6ee45fc34972c1cf055ffde1829d36d79647adc0
Swistek92 <swistekxD@gmail.com>
2021-09-08 05:28:03 +0200

    1

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
-
+const notes = require("./data/notes");
 const app = express();
+
+
+app.get("/",(req,res)=> {
+  res.send("api is runing..")
+});
+
+app.get("/api/notes", (req,res)=>{
+  res.json(notes);
+});
+
 
 app.listen(5000,console.log('server started on port 5000'));
