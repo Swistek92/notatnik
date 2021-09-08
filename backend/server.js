4424ae8fac950493f2836f7a7bc7ed1c962faeed
Swistek92 <swistekxD@gmail.com>
2021-09-08 05:35:54 +0200

    1

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,9 @@ app.get("/api/notes", (req,res)=>{
   res.json(notes);
 });
 
+app.get("/api/notes/:id",(req,res)=>{
+  const note =notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
 
 app.listen(5000,console.log('server started on port 5000'));
