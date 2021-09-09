b266cb3db56d2b9de9c01da1e936cd31947dc449
Swistek92 <swistekxD@gmail.com>
2021-09-09 16:42:13 +0200

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,27 @@ import './App.css';
 import Footer from './components/Foooter/Footer';
 import Header from './components/Header/Header';
 import LandingPage from './screens/LandingPage/LandingPage';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+import MyNotes from './screens/LandingPage/MyNotes/MyNotes';
 
 const App=()=> (
-  <>
+  <Router>
   <Header className="Header"/>
 
-  <main>
-  <LandingPage/>
-
-  </main>
+  <main> 
+  <Route path='/' component={LandingPage} exact /> 
+  <Route path='/mynotes' component={()=> <MyNotes/>} exact /> 
+</main>
+  
 
   <Footer/>
 
-  </>
+  </Router>
 )
 
 export default App;
---
+++ frontend/src/screens/LandingPage/MyNotes/MyNotes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyNotes = () => {
+  return (
+    <div style={{height: "100px", backgroundColor:"red"}}>
+      
+      <h1> My MyNotes</h1> </div>
+  )
+}
+
+export default MyNotes
